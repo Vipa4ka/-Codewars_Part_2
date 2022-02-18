8a30f14d8877410b523b8412358802f81d1a83a1
Your Name <vipa4ka@gmail.com>
2022-02-18 12:11:02 +0200

    21

--- 21.js
+++ 21.js
@@ -36,114 +36,46 @@ const MORSE_TABLE = {
     '----.': '9',
     '-----': '0',
 };
-const expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
 
+const expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
+//  const expr = "00000011110000000010";
 function decode(expr) {
     let l = [];
     let kk = [];
-    let v = [];
     let gf = [];
-    let r = expr.match(/.{1,10}/g);  //разбила по 10 шт
-    // console.log(r)     
+    let r = expr.match(/.{1,10}/g);  //разбила по 10 шт       
     for (let i = 0; i < r.length; i++) {
         r[i] === '**********' ? kk.push(' ') : kk.push(r[i]); //заменила ******** на пробел    
         gf.push(kk[i].match(/.{1,2}/g)); //разбила каждый массив по 2 элем
-
-
-
     }
-    console.log(gf)
-
-    //     gf.map((m, k) => {
-    //         m.map(j => {
-    //             if (j === '10') {
-    //         j = '.';
-    //         v.push(j)
-    //         // console.log(g[i])
-    //     } else if (j === '11') {
-    //                 j = '-';
-    //                 v.push(j)
-    //     }
-    //         })
-    //         console.log(v)
-    // })
-    // console.log(gf) // массив с заменой
-
-
-
-    // for (let i = 0; i < kk.length; i++) {
-    //     let g = kk[i].match(/.{1,2}/g); //разбила каждый массив по 2 элем    
-    //     // console.log(g[i])
-    //     if (g[i] === '10') {
-    //         g[i] = '.';
-    //         // v.push(g[i])
-    //         // console.log(g[i])
-    //     } else if (g[i] === '11') {
-    //         g[i] = '-';
-    //     }
-    //     v.push(g[i])
-    //     //  console.log(v)
-    // }
-    // g.map(d => {
-    //     // console.log(ds);
-    //     if (d === '10') {
-    //         d = '.';
-    //         v.push(d)
-    //         // console.log(d)
-    //     }
-    //     if (d === '11') {
-    //         d = '-';
-    //         v.push(d)
-    //     }
-    //     // v.push(d)
-    // })
-    // }
-    // if (c === '10') {
-    //     c = '.';
-    // console.log(v)
-    // }
-
-    // console.log(g)
-    // let c = g.filter(i => i != 0)
-    // console.log(c)
-    // if (c === '10') {
-    //     c = '.';
-    //     console.log(c)
-    // }
-
-
-
-
-
-
-
-
-    // console.log(r[i]);
-    // console.log(r[i].split('00'))
-
-    // console.log(r[i])
-
-
-
-
-    // let g = r[i].match(/.{1,2}/g)
-    // for (let i = 0; i < g.length; i++) {
-    //     // console.log(g[i])
-    //     if (g[i] === '10') {
-    //         l.push('.')
-
-    //     } else if (g[i] === '11') {
-    //         l.push('-')
-    //     } else if (g[i] === '00') {
-    //         l.push('')
-    //     }
-    //     let h = l[i].match(/.{1,5}/g)
-    //     // console.log(h)
-    // }
-    // }
-    console.log(r[0])
-
+    kk = gf.map(r => {
+        return (r.map(i => {
+            if (i === '10') {
+                i = "."
+            } else if (i === '11') {
+                i = "-";            
+            } else if (i === ' ') {
+                i = ' ';                
+            }            else {
+                i = '';
+            };
+            return i
+        })).join('')        
+    });
+    for (let j = 0; j < kk.length; j++) {
+        for (let key in MORSE_TABLE) {
+            if (kk[j] === key) {
+                l.push(MORSE_TABLE[key])
+            }          
+        }
+        if (kk[j] === ' ') {
+                l.push(' ')
+            }
+    }   
+    return l.join('');
 }
-(decode(expr))
+
+
+console.log (decode(expr))
 
 // console.log(decode(expr));
